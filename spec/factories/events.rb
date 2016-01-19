edfd794421f2632f670d1a24551493679a589dc7
yh2020 <yojihayashi2020@gmail.com>
2016-01-20 02:10:42 +0900

    Railsコマンドでイベントモデルを作成

---
+++ spec/factories/events.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :event do
+    owner_id 1
+name "MyString"
+place "MyString"
+start_time "2016-01-20 02:09:11"
+end_time "2016-01-20 02:09:11"
+content "MyText"
+  end
+
+end
