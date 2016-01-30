543832d09530431411937316da87c993552135e2
yh2020 <yojihayashi2020@gmail.com>
2016-01-31 00:09:09 +0900

    アップロードファイルの拡張子を限定

--- app/uploaders/event_image_uploader.rb
+++ app/uploaders/event_image_uploader.rb
@@ -26,7 +26,7 @@ class EventImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_limit => [1130, 1130]
+  process :resize_to_fit => [1130, 1130]
   # process :scale => [200, 300]
   #
   # def scale(width, height)
@@ -40,9 +40,9 @@ class EventImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
