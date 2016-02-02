f9eb0cd7ade7bc86a1186f5a5d1e6533680898b6
yh2020 <yojihayashi2020@gmail.com>
2016-02-02 16:17:14 +0900

    eventモデルに関するテストを記述

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do
+  describe '#created_by?' do
+    let(:event) { create(:event) }
+    subject { event.created_by?(user) }
+
+    context '引数がnilなとき' do
+      let(:user) { nil }
+      it { is_expected.to be_falsey }
+    end
+
+    context '#owner_idと引数の#idが同じとき' do
+      let(:user) { double('user', id: event.id) }
+      it { is_expected.to be_truthy }
+    end
+  end
 end
