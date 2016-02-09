99c325808da70427850e47921f2644b5e6efebea
yh2020 <yojihayashi2020@gmail.com>
2016-02-09 12:21:20 +0900

    RSpecの構文をシンプルに変更

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -23,27 +23,21 @@ describe Event do
       let(:start_time) { DateTime.new(2020, 8, 9, 00, 00, 00, 'Tokyo') }
       let(:end_time) { DateTime.new(2020, 7, 24, 00, 00, 00, 'Tokyo') }
       let(:event) { build(:event, start_time: start_time, end_time: end_time) }
-      it 'エラーを返すこと' do
-        expect(event).to be_invalid
-      end
+      it { expect(event).to be_invalid }
     end
 
     context '開始時間と終了時間が同じとき' do
       let(:start_time) { DateTime.new(2020, 7, 24, 00, 00, 00, 'Tokyo') }
       let(:end_time) { DateTime.new(2020, 7, 24, 00, 00, 00, 'Tokyo') }
       let(:event) { build(:event, start_time: start_time, end_time: end_time) }
-      it 'エラーを返すこと' do
-        expect(event).to be_invalid
-      end
+      it { expect(event).to be_invalid }
     end
 
     context '開始時間が終了時間より早いとき' do
       let(:start_time) { DateTime.new(2020, 7, 24, 00, 00, 00, 'Tokyo') }
       let(:end_time) { DateTime.new(2020, 8, 9, 00, 00, 00, 'Tokyo') }
       let(:event) { build(:event, start_time: start_time, end_time: end_time) }
-      it 'エラーを返さないこと' do
-        expect(event).to be_valid
-      end
+      it { expect(event).to be_valid }
     end
   end
 end
