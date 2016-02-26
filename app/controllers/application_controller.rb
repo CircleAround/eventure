b3d3a4599ef0872a0f03420780766a651d784d72
yh2020 <yojihayashi2020@gmail.com>
2016-02-26 16:15:21 +0900

    既存アプリとdeviseのすり合わせ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
-
-rescue_from Exception, with: :error500
-rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
 
   private
 
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
-  def logged_in?
-    !!session[:user_id]
-  end
-
   def authenticate
-    return if logged_in?
+    return if user_signed_in?
     redirect_to root_path, alert: 'ログインしてください'
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def url_for_twitter(user)
     "https://twitter.com/#{user.nickname}"
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
