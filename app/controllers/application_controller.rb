183d3d92d31834e4651a23f97c5d5d41bd107caf
yh2020 <yojihayashi2020@gmail.com>
2016-03-01 16:40:20 +0900

    deviseのログインでusernameの入力を求めるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,14 @@ class ApplicationController < ActionController::Base
     logger.error [e, *e.backtrace].join("\n")
     render 'error500', status: 500, formats: [:html]
   end
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
 end
