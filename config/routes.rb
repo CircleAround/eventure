139fb5214532734c2a37dcb99eb9f6616c5b22aa
SideCI <support@sideci.com>
2016-03-08 03:42:05 +0000

    Applied fixes from SideCI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  devise_for :users, :controllers => {
+  devise_for :users, controllers: {
     omniauth_callbacks: 'omniauth_callbacks'
   }
   resources :users, only: :show
