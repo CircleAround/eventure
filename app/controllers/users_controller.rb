cf17f5ec5baa5c2dc3ab1c3ddb7d1ad6c722bb6d
yh2020 <yojihayashi2020@gmail.com>
2016-03-08 14:42:38 +0900

    書式を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
   def show
     @user = User.where(id: current_user).first
 
-    @events_owner = Event.where(owner_id: current_user).where(Event.arel_table[:start_time].gteq Time.zone.now).order(:start_time)
+    @events_owner = Event.where(owner_id: current_user)
+    .where(Event.arel_table[:start_time]
+    .gteq(Time.zone.now))
+    .order(:start_time)
 
-    @events_part = Event.where(Event.arel_table[:start_time].gteq Time.zone.now).joins(:tickets).merge(Ticket.where(user_id: current_user)).order(:start_time)
+    @events_part = Event.where(Event.arel_table[:start_time]
+    .gteq(Time.zone.now))
+    .joins(:tickets)
+    .merge(Ticket.where(user_id: current_user))
+    .order(:start_time)
   end
 
   def retire
