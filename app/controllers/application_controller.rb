4e9f21db57c8dc10ba824a5827e76200e0f674d6
yh2020 <yojihayashi2020@gmail.com>
2016-03-10 16:29:26 +0900

    rubocopに指摘された部分を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: 'ログインしてください'
   end
 
-  def error404(e)
+  def error404(*)
     render 'error404', status: 404, formats: [:html]
   end
 
@@ -22,13 +22,19 @@ class ApplicationController < ActionController::Base
     render 'error500', status: 500, formats: [:html]
   end
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u|
+      u.permit(:username, :email, :password, :password_confirmation, :remember_me)
+    }
+    devise_parameter_sanitizer.for(:sign_in) { |u|
+      u.permit(:login, :username, :email, :password, :remember_me)
+    }
+    devise_parameter_sanitizer.for(:account_update) { |u|
+      u.permit(:username, :email, :password, :password_confirmation, :current_password)
+    }
+  end
 end
