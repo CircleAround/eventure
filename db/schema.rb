cb59b0848b88ecd86289238238f019d181f8c2ec
yh2020 <yojihayashi2020@gmail.com>
2016-03-15 17:33:44 +0900

    eventsテーブルにdeleted_atカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315072431) do
+ActiveRecord::Schema.define(version: 20160315081721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20160315072431) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "event_image"
+    t.datetime "deleted_at"
   end
 
+  add_index "events", ["deleted_at"], name: "index_events_on_deleted_at", using: :btree
   add_index "events", ["owner_id"], name: "index_events_on_owner_id", using: :btree
 
   create_table "tickets", force: :cascade do |t|
